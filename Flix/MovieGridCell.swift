8cee46d842de0b318a032f7ac2e5df93f58bb2e5
tsugita26 <tsugita@ucsd.edu>
2021-09-26 12:56:14 -0700

    second commit

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Sugita on 9/25/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+
+    
+}
